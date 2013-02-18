0553f1829addc3df9012c6d0f354674c8f0b33ae
Patrick Rock <prock2011@gmail.com>
2013-02-17 17:55:08 -0800

    added documnet converter

--- homework2/A4.hs
+++ homework2/A4.hs
@@ -14,7 +14,20 @@ makeSection s = "<section>" ++ s ++ "</section>"
 makeSubsection :: String -> String
 makeSubsection s = "<section>" ++ s ++ "</section>"
 
+makePlaintext :: String -> String
+makePlaintext s = s
+
+makeBlock :: String -> String
+makeBlock s = s
 -----------------------------
+
+convertDoc :: [(Keywords, String)] -> String
+converDoc x:xs 
+	| fst x == Title      = makeTitle (snd x)      : (convertDoc xs)
+	| fst x == Section    = makeSection (snd x)    : (convertDoc xs)
+	| fst x == Subsection = makeSubsection (snd x) : (convertDoc xs)
+	| fst x == Block      = makeBlock (snd x)      : (convertDoc xs)
+	| fst x == Plaintext  = makePlaintext(snd x)   : (convertDoc xs)
 
 
 
