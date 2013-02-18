728fd8d70b1d64063a09dbbda980b4a76cc73c23
Patrick Rock <prock2011@gmail.com>
2013-02-17 17:44:58 -0800

    added make functions

--- homework2/A4.hs
+++ homework2/A4.hs
@@ -4,6 +4,20 @@ import Data.List
 data KeyWords = Title | Section | Subsection | Block | Plaintext
 
 
+-- CONVERTER FUNCTIONS --
+makeTitle :: String -> String 
+makeTitle s = "<title>" ++ s ++ "</title>"
+
+makeSection :: String -> String
+makeSection s = "<section>" ++ s ++ "</section>"
+
+makeSubsection :: String -> String
+makeSubsection s = "<section>" ++ s ++ "</section>"
+
+-----------------------------
+
+
+
 isKeyWord :: String -> Bool
 isKeyWord s | head s ==  '@' = True
 			| otherwise = False
