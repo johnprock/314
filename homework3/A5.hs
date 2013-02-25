6a0b865f69dcbf7923d84dfedd273bd34ff8d762
Patrick Rock <prock2011@gmail.com>
2013-02-24 21:31:43 -0600

    added parser monad

--- homework3/A5.hs
+++ homework3/A5.hs
@@ -1,5 +1,21 @@
 import System.Environment
 
+newtype Parser a = Parser (String -> [(a,String)])
+
+parse :: Parser a -> String -> [(a,String)]
+parse (Parser p) inp = p inp
+
+instance Monad Parser where
+   return v = Parser (\inp -> [(v,inp)])
+   p >>= f =  Parser (\inp -> case parse p inp of
+                         []        -> []
+                         [(v,out)] -> parse (f v) out)
+
+data Grammar = Operator | Digit 
+
+                   
+
+--copy input to output
 echo :: IO ()
 echo = do
        line <- getLine
