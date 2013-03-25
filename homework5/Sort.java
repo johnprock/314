e7f0f08212f003cfce43eca7bdcb91c4c52dfdaa
johnprock <patrickrock@neo.tamu.edu>
2013-03-25 12:49:47 -0500

    laid out code skeleton

---
+++ homework5/Sort.java
@@ -0,0 +1,30 @@
+class Sort {
+
+  private abstract class Algorithm {
+  }
+
+  private class Bubble {
+  }
+
+  private class Insertion {
+  }
+
+  private class Shell {
+  }
+
+  private class Merge {
+  }
+
+  private class Quick {
+  }
+
+  private class Heap {
+  }
+
+  private class Visual {
+  }
+
+  public static void main(String[] args) {
+  }
+
+}
