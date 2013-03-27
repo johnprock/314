ca8e61904912d88457060cf69260d1a9abe27e50
johnprock <patrickrock@neo.tamu.edu>
2013-03-27 14:50:07 -0500

    ...

---
+++ homework5/AnimateSorting.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+class AnimateSorting {
+
+  public static void main(String[] args) {
+    JFrame win = new JFrame();
+    win.setDefaultCloseOperation(TextFrame.EXIT_ON_CLOSE);
+    win.setBounds(10,10,900,200);
+  }
+
+}
