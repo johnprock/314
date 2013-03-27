0c7a30dd9288f2a4a8224440e17283ab8666dc98
johnprock <patrickrock@neo.tamu.edu>
2013-03-27 15:05:52 -0500

    statred threaded design

--- homework5/AnimateSorting.java
+++ homework5/AnimateSorting.java
@@ -9,9 +9,10 @@ import javax.swing.*;
 class AnimateSorting {
 
   public static void main(String[] args) {
-    JFrame win = new JFrame();
-    win.setDefaultCloseOperation(TextFrame.EXIT_ON_CLOSE);
-    win.setBounds(10,10,900,200);
+    new AnimateSorting(); 
+  }
+
+  public AnimateSorting() {
   }
 
 }
