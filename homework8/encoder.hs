ba60a4e051f40cc48b090eb743c3ec845dcfe1ef
johnprock <prock2011@gmail.com>
2013-04-27 23:15:19 -0500

    structured program assigned jobs

--- homework8/encoder.hs
+++ homework8/encoder.hs
@@ -4,15 +4,24 @@
 -- Patrick Rock:    ROT13
 
 import System.Environment
+import Data.String
 
+-- Patrck's Function
+rot13 :: String -> String
+rot13 s = "rot13"
 
-encrypt :: String -> String -> String
-encrypt s opt = " "
+-- Kalil's Function
+rc4 :: String -> String
+rc4 s = "rc4"
 
+encrypt :: String -> (String -> String)
+encrypt opt
+  | opt == "rc4"    = rc4
+  | opt == "rot13"  = rot13
 
 main :: IO()
 main = do
   [inp,out,opt] <- getArgs
   s         <- readFile inp
-  writeFile out (encrypt s opt)
+  writeFile out (encrypt opt s)
 
